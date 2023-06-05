759136b6ba6093ad1f6d2168fe50226ad7d8264b
wf910 <135550177+wf910@users.noreply.github.com>
2023-06-05 17:47:06 +1000

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -18,19 +18,19 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
 // Set Prices
 function kcal(exp) {
   var kcal = [];
-  kcal[2]=200;
-  kcal[4]=250;
-  kcal[8]=320;
-  kcal[16]=400;
-  kcal[32]=500;
-  kcal[64]=650;
-  kcal[128]=820;
-  kcal[256]=1000;
-  kcal[512]=1200;
-  kcal[1024]=1500;
-  kcal[2048]=2000;
-  kcal[4096]=3000;
-  kcal[8192]=5000;
+  kcal[2]=2;
+  kcal[4]=4;
+  kcal[8]=8;
+  kcal[16]=16;
+  kcal[32]=32;
+  kcal[64]=64;
+  kcal[128]=128;
+  kcal[256]=256;
+  kcal[512]=512;
+  kcal[1024]=1024;
+  kcal[2048]=2048;
+  kcal[4096]=4096;
+  kcal[8192]=8192;
   return kcal[exp];
 }
 
